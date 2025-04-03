7ec31e5e725acfdb1c5ea3caa70062cd7cae6aa1
borislavr <boris.lavr@gmail.com>
2025-04-03 15:01:14 +0300

    feat: Enhance Maven multi-module release workflow with improved logging and dependency management

--- .github/actions/maven-multimodule-release/action-scripts1.sh
+++ .github/actions/maven-multimodule-release/action-scripts1.sh
@@ -25,10 +25,11 @@ function bump_version_and_build() {
     cd ${GITHUB_WORKSPACE}/${MODULE}
     git config --global user.name "${GITHUB_ACTOR}"
     git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-    echo "Bumping ${MODULE} version"
     if [ "${DRY_RUN}" != "false" ]; then
+        echo "::group::Buildin ${MODULE} current version."
         echo "Dry run. Not bumping version."
         mvn --batch-mode deploy $MVN_ARGS
+        echo "::endgroup::"
         if [ $? -ne 0 ]; then
             echo "Build failed. Exiting."
             echo "❌ Dry-run: build ${MODULE} version ${RELEASE_VERSION} failsed." >> $GITHUB_STEP_SUMMARY
@@ -37,8 +38,10 @@ function bump_version_and_build() {
         export RELEASE_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
         echo "✔️ Dry-run: Successfully built ${MODULE} version ${RELEASE_VERSION}" >> $GITHUB_STEP_SUMMARY
     else
+        echo "::group::Preparing ${MODULE} release."
         mvn --batch-mode versions:use-releases -DgenerateBackupPoms=false
-        mvn --batch-mode release:prepare -DautoVersionSubmodules=true -DpushChanges=true -DtagNameFormat="v@{project.version}"
+        mvn --batch-mode release:prepare -DautoVersionSubmodules=true -DpushChanges=false -DtagNameFormat="v@{project.version}"
+        echo "::endgroup::"
         if [ $? -ne 0 ]; then
             echo "Release preparation failed. Exiting."
             echo "❌ Release: preparation of ${MODULE} version ${RELEASE_VERSION} release failed." >> $GITHUB_STEP_SUMMARY
@@ -54,11 +57,49 @@ function bump_version_and_build() {
         echo "Dry run. Not committing."
         return
     fi
+    echo "::group::Releasing ${MODULE} version ${RELEASE_VERSION}"
     mvn --batch-mode release:perform -DpushChanges=true
+    echo "::endgroup::"
     if [ $? -ne 0 ]; then
         echo "Release perform failed. Exiting."
         echo "❌ Release: ${MODULE} version ${RELEASE_VERSION} releas failed." >> $GITHUB_STEP_SUMMARY
         exit 1
     fi
     echo "✅ Release: ${MODULE} version ${RELEASE_VERSION} released successfully." >> $GITHUB_STEP_SUMMARY
+    # To update pom.xml dependencies with the next -SNAPSHOT version need to deploy SNAPSHOT version
+    if [ "${DRY_RUN}" != "false" ]; then
+        echo "Dry run. Not updating dependencies."
+        return
+    fi
+    echo "::group::Building ${MODULE} version ${RELEASE_VERSION}-SNAPSHOT"
+    mvn --batch-mode deploy -DskipTests=true $MVN_ARGS
+    echo "::endgroup::"
+    if [ $? -ne 0 ]; then
+        echo "Build failed. Exiting."
+        echo "❌ Build: ${MODULE} version ${RELEASE_VERSION}-SNAPSHOT failed." >> $GITHUB_STEP_SUMMARY
+        exit 1
+    fi
+    echo "✅ Build: ${MODULE} version ${RELEASE_VERSION}-SNAPSHOT built successfully." >> $GITHUB_STEP_SUMMARY
+    echo "::group::Updating ${MODULE} dependencies versions to next-snapshot"
+    mvn --batch-mode versions:use-next-snapshots -DgenerateBackupPoms=false -Dincludes="org.qubership.cloud*:*"
+    echo "::endgroup::"
+    if [ $? -ne 0 ]; then
+        echo "Update dependencies failed. Exiting."
+        echo "❌ Update: ${MODULE} dependencies versions to next-snapshot failed." >> $GITHUB_STEP_SUMMARY
+        exit 1
+    fi
+    echo "✅ Update: ${MODULE} dependencies versions to next-snapshot updated successfully." >> $GITHUB_STEP_SUMMARY
+    echo "::group::Clean and commit pom.xml with next-snapshot version."
+    echo "Committing pom.xml with release version."
+    mvn --batch-mode clean
+    git add .
+    git commit -m "Bump dependencies versions to next-snapshot [skip ci]"
+    git push
+    echo "::endgroup::"
+    if [ $? -ne 0 ]; then
+        echo "Commit failed. Exiting."
+        echo "❌ Commit: ${MODULE} pom.xml with next-snapshot version failed." >> $GITHUB_STEP_SUMMARY
+        exit 1
+    fi
+    echo "✅ Commit: ${MODULE} pom.xml with next-snapshot version committed successfully." >> $GITHUB_STEP_SUMMARY
 }
